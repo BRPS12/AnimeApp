e89acd4638d6a1d74775e844235f537855ef0244
Болд-Эрдэнэ Батбаяр <HP239289@G302-CC0076.local>
2024-01-29 18:55:38 +0800

    s

--- app/anime/[id].tsx
+++ app/anime/[id].tsx
@@ -8,6 +8,8 @@ import {
   Button,
   FlatList,
   ScrollView,
+  TouchableOpacity,
+  TouchableHighlight,
 } from "react-native";
 import { useEffect, useState } from "react";
 import { Link, useGlobalSearchParams, useLocalSearchParams } from "expo-router";
@@ -15,10 +17,55 @@ import { Link, useGlobalSearchParams, useLocalSearchParams } from "expo-router";
 export default function sdasd() {
   const { id } = useGlobalSearchParams();
   const [data, setData] = useState<any>();
-  const [episode, setEpisode] = useState<any>([]);
+  const [alldata, setAllData] = useState();
+  const [episode, setEpisode] = useState<any>([]) || null;
   const [loading, setLoading] = useState(true);
+  const [selectedButton, setSelectedButton] = useState(null);
 
+  const handleButtonPress = (buttonName: any) => {
+    setSelectedButton(buttonName);
+  };
+  const renderButton = (buttonName: any, label: any) => (
+    <TouchableHighlight
+      key={buttonName}
+      style={{
+        width: "40%",
+        height: 45,
+        flex: 1,
+        alignItems: "center",
+      }}
+      onPress={() => handleButtonPress(buttonName)}
+      underlayColor="transparent"
+    >
+      <View>
+        <Text
+          style={{
+            width: 190,
+            height: 38,
+            textAlign: "center",
+            fontSize: selectedButton === buttonName ? 20 : 20,
+            color: selectedButton === buttonName ? "#00EC59" : "black",
+            fontWeight: selectedButton === buttonName ? "bold" : "normal",
+            textDecorationLine:
+              selectedButton === buttonName ? "underline" : "underline",
+          }}
+        >
+          {label}
+        </Text>
+      </View>
+    </TouchableHighlight>
+  );
   useEffect(() => {
+    fetch(`https://kitsu.io/api/edge/anime/`)
+      .then((res) => res.json())
+      .then(({ data }) => {
+        setAllData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+
     fetch(`https://kitsu.io/api/edge/anime/${id}`)
       .then((res) => res.json())
       .then(({ data }) => {
@@ -38,6 +85,7 @@ export default function sdasd() {
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
+    handleButtonPress("more");
   }, []);
   if (loading) {
     return (
@@ -70,14 +118,33 @@ export default function sdasd() {
         </ImageBackground>
       </View>
       <View style={{ padding: 0, height: "100%", flex: 1 }}>
-        <View style={{ flexDirection: "row", alignItems: "center" }}>
-          <Text style={{ fontWeight: "700", fontSize: 24, margin: 15 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            width: 300,
+            height: 70,
+          }}
+        >
+          <Text style={{ fontWeight: "700", fontSize: 24, marginLeft: 15 }}>
             {data.attributes.titles.en_jp}
           </Text>
-          <View style={{ flexDirection: "row", marginLeft: 40 }}>
-            <Button title="Neg" />
-            <Button title="hoyr" />
-          </View>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            position: "absolute",
+            right: 20,
+            top: 25,
+          }}
+        >
+          <Image source={require("../Images/Bookmark.png")} />
+          <Image
+            source={require("../Images/Send.png")}
+            style={{
+              marginLeft: 15,
+            }}
+          />
         </View>
         <View>
           <View
@@ -129,13 +196,13 @@ export default function sdasd() {
             <View
               style={{
                 position: "absolute",
-                right: 50,
+                right: 10,
                 width: 170,
                 height: 40,
                 borderColor: "#06C149",
                 borderRadius: 20,
                 borderWidth: 1.5,
-                marginRight: 15,
+                marginRight: 5,
               }}
             >
               <Button title="Download" color="#06C149" />
@@ -166,10 +233,18 @@ export default function sdasd() {
                   horizontal={true}
                   renderItem={({ item }: { item: any }) => (
                     <View style={{ marginTop: 10, marginLeft: 15 }}>
-                      <Image
-                        source={{ uri: item.attributes.thumbnail.original }}
-                        style={{ width: 150, height: 113, borderRadius: 10 }}
-                      />
+                      {item.attributes.thumbnail ? (
+                        <Image
+                          source={{
+                            uri: item.attributes.thumbnail.original || null,
+                          }}
+                          style={{ width: 150, height: 113, borderRadius: 10 }}
+                        />
+                      ) : (
+                        <View>
+                          <Text>Sorry we do not have this yet.</Text>
+                        </View>
+                      )}
                     </View>
                   )}
                 />
@@ -179,11 +254,64 @@ export default function sdasd() {
                   flexDirection: "row",
                   alignItems: "center",
                   justifyContent: "center",
+                  marginTop: 20,
+                  flex: 1,
                 }}
               >
-                <Text style={{ width: 190, height: 38 }}>More like this</Text>
-                <Text style={{ width: 190, height: 38 }}>Comments</Text>
+                {renderButton("more", "More like this")}
+                {renderButton("comments", "Comments")}
               </View>
+              {selectedButton === "more" &&
+                alldata &&
+                alldata.map((item: any, i: any) => {
+                  return (
+                    <View key={i}>
+                      <View
+                        style={{
+                          marginLeft: 35,
+                          justifyContent: "center",
+                          padding: 0,
+                          marginTop: 10,
+                          borderRadius: 10,
+                        }}
+                      >
+                        <ImageBackground
+                          source={{ uri: item.attributes.posterImage.tiny }}
+                          style={{ width: 110, height: 156, borderRadius: 10 }}
+                        >
+                          <View
+                            style={{
+                              height: 20,
+                              width: 40,
+                              backgroundColor: "#00EC59",
+                              borderRadius: 7,
+                              margin: 10,
+                            }}
+                          >
+                            <Text
+                              style={{ textAlign: "center", color: "#fff" }}
+                            >
+                              {item.attributes.averageRating}
+                            </Text>
+                          </View>
+                        </ImageBackground>
+                      </View>
+                    </View>
+                  );
+                })}
+              {selectedButton === "comments" && (
+                <View>
+                  <Text>Comment</Text>
+                  <Text>Comment</Text>
+                  <Text>Comment</Text>
+                  <Text>Comment</Text>
+                  <Text>Comment</Text>
+                  <Text>Comment</Text>
+                  <Text>Comment</Text>
+                  <Text>Comment</Text>
+                  <Text>Comment</Text>
+                </View>
+              )}
             </View>
           </View>
         </View>
